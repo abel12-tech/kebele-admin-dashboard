9d8e48183562083ff913b08020f233f966b34061
abel <abelkinfu678@gmail.com>
2024-06-18 17:08:24 +0300

    sth is updated

--- src/components/features/manage-requests/pages/ManageRequestsInKebele.jsx
+++ src/components/features/manage-requests/pages/ManageRequestsInKebele.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDarkMode } from "../../../../shared/darkModeContext";
 import {
   useDeleteRequestMutation,
-  useGetAllRequestsQuery,
+  useGetAllRequestInKebeleQuery,
   useUpdateStatusMutation,
 } from "../api/requestsApi";
 
@@ -14,7 +14,8 @@ const ManageRequestsInKebele = () => {
     isLoading,
     isSuccess,
     error,
-  } = useGetAllRequestsQuery();
+  } = useGetAllRequestInKebeleQuery();
+  console.log(requests)
 
   const [updateStatus] = useUpdateStatusMutation();
 
@@ -60,10 +61,15 @@ const ManageRequestsInKebele = () => {
     setCurrentPage(page);
   };
 
-  const totalPages = Math.ceil((requests?.length || 0) / itemsPerPage);
+  const totalPages = Math.ceil(
+    (requests?._idsInKebele.length || 0) / itemsPerPage
+  );
 
   const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = Math.min(startIndex + itemsPerPage, requests?.length || 0);
+  const endIndex = Math.min(
+    startIndex + itemsPerPage,
+    requests?._idsInKebele.length || 0
+  );
 
   return (
     <div
@@ -103,46 +109,50 @@ const ManageRequestsInKebele = () => {
                     </td>
                   </tr>
                 ) : isSuccess ? (
-                  requests.slice(startIndex, endIndex).map((request) => (
-                    <tr
-                      key={request._id}
-                      className={`${
-                        isDarkMode ? "text-gray-400" : "text-gray-700"
-                      }`}
-                    >
-                      <td className="px-4 py-3 text-sm">{request.resident}</td>
-                      <td className="px-4 py-3 text-sm">{request.status}</td>
-                      <td className="px-4 py-3 text-sm">
-                        <div className="flex items-center space-x-4 text-sm">
-                          <button
-                            className="flex items-center bg-green-400 text-white justify-between px-2 py-2 text-sm font-medium leading-5 rounded-lg"
-                            aria-label="Change Status"
-                            onClick={() => onChangeStatus(request._id)}
-                          >
-                            changeStatus
-                          </button>
-                          <button
-                            className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                            aria-label="Delete"
-                            onClick={() => onDelete(request._id)}
-                          >
-                            <svg
-                              className="w-5 h-5"
-                              aria-hidden="true"
-                              fill="currentColor"
-                              viewBox="0 0 20 20"
+                  requests?._idsInKebele
+                    .slice(startIndex, endIndex)
+                    .map((request) => (
+                      <tr
+                        key={request._id}
+                        className={`${
+                          isDarkMode ? "text-gray-400" : "text-gray-700"
+                        }`}
+                      >
+                        <td className="px-4 py-3 text-sm">
+                          {request.resident}
+                        </td>
+                        <td className="px-4 py-3 text-sm">{request.status}</td>
+                        <td className="px-4 py-3 text-sm">
+                          <div className="flex items-center space-x-4 text-sm">
+                            <button
+                              className="flex items-center bg-green-400 text-white justify-between px-2 py-2 text-sm font-medium leading-5 rounded-lg"
+                              aria-label="Change Status"
+                              onClick={() => onChangeStatus(request._id)}
                             >
-                              <path
-                                fillRule="evenodd"
-                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
-                        </div>
-                      </td>
-                    </tr>
-                  ))
+                              changeStatus
+                            </button>
+                            <button
+                              className="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                              aria-label="Delete"
+                              onClick={() => onDelete(request._id)}
+                            >
+                              <svg
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fillRule="evenodd"
+                                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
                 ) : (
                   <tr>
                     <td
@@ -165,7 +175,8 @@ const ManageRequestsInKebele = () => {
             }  uppercase border-t  sm:grid-cols-9`}
           >
             <span className="flex items-center col-span-3">
-              Showing {startIndex + 1}-{endIndex} of {requests?.length}
+              Showing {startIndex + 1}-{endIndex} of{" "}
+              {requests?._idsInKebele.length}
             </span>
             <span className="col-span-2" />
             {/* Pagination */}
